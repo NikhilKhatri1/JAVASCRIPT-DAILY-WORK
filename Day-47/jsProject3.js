bb6def9865280d8f2c243d72ec711ba19f19e8f0
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2024-09-30 01:48:32 +0530

    .

--- Day-47/jsProject3.js
+++ Day-47/jsProject3.js
@@ -1,3 +1,5 @@
+const { listenerCount } = require("process")
+
 const prompt = require("prompt-sync")()
 
 function ShowInfo() {
@@ -12,21 +14,56 @@ function ShowInfo() {
 }
 
 function AddContact() {
-
+    const name = prompt("Name : ")
+    const email = prompt("Email : ")
+    const contact = {
+        name: name,
+        email: email
+    }
+    contacts.push(contact)
+    console.log("Contact Added...")
 }
 function DeleteContact() {
-
+    console.log("Contact Ids")
+    for (let i = 0; i < contacts.length; i++) {
+        const contact = contacts[i]
+        console.log(`${(i + 1).toString()} : ${contact.name}`)
+    }
+    const number = parseInt(prompt("Enter the id : "))
+    if (isNaN(number) || number > contacts.length) {
+        console.log("Invalid input...")
+        return
+    }
+    contacts.splice(number - 1, 1)
+    console.log("Contact Removed....")
 }
 
-function ViewContact() {
+function ViewContact(contacts) {
+    if (contacts.length === 0) {
+        console.log("No Available Contacts ")
+    }
+    else {
+        contacts.map(contact => {
+            console.log("+++++++++++++++++++++");
+            console.log(`Name : ${contact.name}`)
+            console.log(`Email : ${contact.email}`)
 
+        })
+    }
 }
 function SearchContact() {
-
+    const SearchString = prompt("Search Contact : ").toLowerCase();
+    const result = [];
+    for (let contact of contacts) {
+        if (contact.name.toLowerCase().includes(SearchString)) {
+            result.push(contact);
+        }
+        ViewContact(result)
+    }
 }
-ShowInfo()
-
+const contacts = []
 const flag = true;
+ShowInfo()
 
 while (flag) {
     const number = prompt("Enter the number (1-5) : ")
@@ -38,7 +75,7 @@ while (flag) {
             DeleteContact();
             break;
         case "3":
-            ViewContact();
+            ViewContact(contacts);
             break;
         case "4":
             SearchContact();
