b4dd5488b5bb40c0be5fddcc7588ca2a4aadd44b
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2024-11-09 14:09:24 +0530

    .

--- Day-51/app.js
+++ Day-51/app.js
@@ -1,1 +1,13 @@
-alert("Hello World!")
+let x = 10;
+function varShadow() {
+    let x = 20;
+    console.log(x)   // 20
+    if (true) {
+        let x = 30;
+        console.log(x)    // 30
+    }
+    console.log(x)  //20
+
+}
+varShadow()
+console.log(x)   //10
