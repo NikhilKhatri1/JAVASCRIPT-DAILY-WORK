50cfd7b0e6130c400b7c8cf5173ea4f1f59ed2ed
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2024-12-13 21:49:28 +0530

    questions of js

---
+++ Day-55/JsQuestion/q1.js
@@ -0,0 +1,50 @@
+// "use strict";
+
+
+// {
+//     function myfn() {
+//         console.log("hi")
+//     }
+// }
+
+// myfn();
+
+// {
+//     const myfn = () => {
+//         console.log("hi")
+//     }
+// }
+// myfn();
+// referrence error:
+
+
+// a and b are already parameters of the function. //
+//function add(a, b) {
+    // "use strict";
+    // In strict mode, you cannot redeclare parameters as local variables with the same name //
+   // let a = 100, b = 200;
+    //return arguments[0] + arguments[1];
+    // The return statement will stop the function execution immediately, //
+    // so the console.log(a + b); will not execute //
+    //console.log(a + b);
+// }
+
+//console.log(add(10, 20));
+
+// a and b are already parameters of the function. //
+// "use strict";
+// function add(a, b) {
+//     // In strict mode, you cannot redeclare parameters as local variables with the same name //
+//     var a = 100, b = 200;
+//     return arguments[0] + arguments[1];
+//     // The return statement will stop the function execution immediately, //
+//     // so the console.log(a + b); will not execute //
+//     console.log(a + b);
+// }
+
+// console.log(add(10, 20));   // 30
+
+
+
+
+
