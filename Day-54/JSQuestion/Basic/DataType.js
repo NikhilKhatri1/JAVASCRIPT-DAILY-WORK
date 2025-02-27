d636270d40c9c5208f852708b872f2a953e18f86
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2025-02-27 18:37:59 +0530

    .

---
+++ Day-54/JSQuestion/Basic/DataType.js
@@ -0,0 +1,14 @@
+// coercion
+
+var add = '5' + 5;
+// console.log(add);
+
+var divide = 'string' % 5;
+// console.log(divide);
+
+
+var a;
+console.log(a);   // undefined
+a = 10;
+console.log(a);  // 10
+
