a64deaa464b14f94dd3b45dd1734cc49724a49ee
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2025-03-04 05:56:23 +0530

    .

---
+++ Day-54/JSQuestion/Basic/Object.js
@@ -0,0 +1,9 @@
+// how to create Object
+
+let myObj = {
+    name: "Anil",
+    class: "12th",
+    subject: ["Math", "English", "Physics", "Chemistry"]
+}
+
+console.log("My Object Name : ", myObj.name);
