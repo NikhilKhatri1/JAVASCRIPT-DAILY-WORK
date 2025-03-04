b6ae9e315c6799d10056151d31557c9881adc62a
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2025-03-04 17:13:23 +0530

    .

--- Day-54/JSQuestion/Basic/Object.js
+++ Day-54/JSQuestion/Basic/Object.js
@@ -3,7 +3,39 @@
 let myObj = {
     name: "Anil",
     class: "12th",
-    subject: ["Math", "English", "Physics", "Chemistry"]
+    subject: ["Math", "English", "Physics", "Chemistry"],
+    greet: function () {
+        console.log(`Hello, ${this.name}`);
+    }
 }
 
-console.log("My Object Name : ", myObj.name);
+myObj.greet();
+
+function ListOfSubject() {
+    let count = 0;
+    let sub = myObj.subject;
+    sub.map(s => {
+        console.log(`Subject = ${s} and count = ${count}`);
+        count++;
+    })
+
+}
+
+ListOfSubject();
+
+
+// ==== add properties in object ===== //
+
+let myObj1 = {
+    name: 'bob',
+    age: 22
+}
+
+console.log(myObj1)
+
+myObj1.hobby = "Cricket"
+
+console.log(myObj1)
+
+// iterate object using Object.entries
+Object.entries(myObj1).map(prop=>console.log(prop))
